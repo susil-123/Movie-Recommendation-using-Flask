22cc10e9da95e50727a6d85a47857cad42d08415
susil kumar <susilkumarkct@gmail.com>
2024-06-21 21:35:20 +0530

    updated requirements.py

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import os
+import pickle
+import sys
+from src.exception import CustomException
+
+def save_obj(file_path,obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,'wb') as file:
+            pickle.dump(obj,file)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
